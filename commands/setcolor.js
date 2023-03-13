adc47eb0f2692224594447038398dafed54f8740
Evidentsinger14 <Cdelancey186423@gmail.com>
2023-03-13 18:28:36 -0400

    maybe??

--- commands/setcolor.js
+++ commands/setcolor.js
@@ -1,14 +1,17 @@
 module.exports.run = async (client, message, args) =>{
 
     let color = "#000000";
-    switch (args[0]){
-        case "random":
-            let random = color.replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-            message.channel.send(random);
-            break;
-        default:
-            color = args[0];
-            message.channel.send(color);
-            break;
+
+
+    function clrGen(args){
+        let clr;
+        if(args[0] === "random"){
+            clr = color.replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+        } else {
+            clr = args[0];
+        }
+        return clr;
     }
+    message.channel.send(clrGen(args[0]));
+
 }
