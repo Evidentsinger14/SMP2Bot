ffaf3856ed0ebef4a86b1b6188a89fc18adf746c
Evidentsinger14 <Cdelancey186423@gmail.com>
2023-03-13 18:36:25 -0400

    remove that debug code, this works.

--- commands/setcolor.js
+++ commands/setcolor.js
@@ -4,11 +4,9 @@ module.exports.run = async (client, message, args) =>{
     switch (args[0]){
         case "random":
             clr = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-            message.channel.send(clr);
             break;
         default:
             clr = args[0];
-            message.channel.send(clr);
             break;
     }
     message.channel.send("Your color is " + clr)
