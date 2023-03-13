0c3aedb1025398df8543527852018d2b657c2a8e
Evidentsinger14 <Cdelancey186423@gmail.com>
2023-03-13 17:59:53 -0400

    base code for command

--- commands/setcolor.js
+++ commands/setcolor.js
@@ -1,3 +1,10 @@
-module.exports.run = async (client, message) =>{
-    message.reply("AAAAAAAAAAAAH");
+module.exports.run = async (client, message, args) =>{
+    switch (args[0]){
+        case "random":
+            message.channel.send("Random Color")
+            break;
+        default:
+            message.channel.send("A")
+            break;
+    }
 }
