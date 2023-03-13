c318ae937fd1e8382b290559972c539a29d78832
Evidentsinger14 <Cdelancey186423@gmail.com>
2023-03-13 18:08:33 -0400

    maybe it's not getting updated properly?

--- commands/setcolor.js
+++ commands/setcolor.js
@@ -3,8 +3,8 @@ module.exports.run = async (client, message, args) =>{
     let color = "#000000";
     switch (args[0]){
         case "random":
-            color.replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-            message.channel.send(color);
+            let random = color.replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+            message.channel.send(random);
             break;
         default:
             color = args[0];
