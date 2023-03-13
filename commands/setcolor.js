40702ca20b385b79e498c4103974414dec701178
Evidentsinger14 <Cdelancey186423@gmail.com>
2023-03-13 18:05:20 -0400

    handle args for colors.

--- commands/setcolor.js
+++ commands/setcolor.js
@@ -1,10 +1,13 @@
 module.exports.run = async (client, message, args) =>{
+
+    let color = "#000000";
     switch (args[0]){
         case "random":
-            message.channel.send("Random Color")
+            color.replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+            message.channel.send(color)
             break;
         default:
-            message.channel.send("A")
+            color = args[0];
             break;
     }
 }
